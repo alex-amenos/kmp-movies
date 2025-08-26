d638bb3437e3c9b7d3a44df16b3acd931ac6991b
Alex <alxnophis@pm.me>
2025-08-27 00:22:05 +0200

    update: upgrade Gradle and dependencies versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,18 +16,18 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
         }
     }
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
@@ -51,12 +51,21 @@ kotlin {
 
 android {
     namespace = "com.alxnophis.kmpmovies"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk =
+        libs.versions.android.compileSdk
+            .get()
+            .toInt()
 
     defaultConfig {
         applicationId = "com.alxnophis.kmpmovies"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk =
+            libs.versions.android.minSdk
+                .get()
+                .toInt()
+        targetSdk =
+            libs.versions.android.targetSdk
+                .get()
+                .toInt()
         versionCode = 1
         versionName = "1.0"
     }
@@ -79,4 +88,3 @@ android {
 dependencies {
     debugImplementation(compose.uiTooling)
 }
-
