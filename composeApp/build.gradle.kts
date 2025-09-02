81b51d16a16b513918413aa833f9322702013191
Alex <alxnophis@pm.me>
2025-09-02 23:34:42 +0200

    feat: add androidx.navigation.compose dependency for navigation support

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -46,6 +46,7 @@ kotlin {
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor)
             implementation(libs.ktor.client.core)
+            implementation(libs.androidx.navigation.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
